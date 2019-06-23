cef4b53ce76a65c95ed50d45932455e188cfc7bd
RADEN-ies <50079895+RADEN-ies@users.noreply.github.com>
2019-06-23 23:01:30 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo -e '\033[0;36m#############	\033[91m| \033[0mAuto Push Rank mirror-h.org';
-echo -e '\033[0;36m# \033[93m|P|U|S|H| \033[0;36m#	\033[91m| \033[92m@Author : Gambrush';
-echo -e '\033[0;36m# \033[93m|R|A|N|K| \033[0;36m#	\033[91m| \033[92m@Github : https://github.com/gambrush'; 
+echo -e '\033[0;36m#############	\033[91m| \033[0mAuto Push fake Rank mirror-h.org';
+echo -e '\033[0;36m# \033[93m|PUSH| \033[0;36m#	\033[91m| \033[92m@Author : MR.W4HYU';
+echo -e '\033[0;36m# \033[93m|DONG| \033[0;36m#	\033[91m| \033[92m@Github : https://github.com/RADEN-ies'; 
 echo -e '\033[0;36m#############	\033[91m| \033[92m@Version : 1.0';
 echo '';
 echo -e 'Pilih Opsi:';
-echo -e '\033[91m[\033[0m1\033[91m] \033[92mSubdomain Generator';
+echo -e '\033[91m[\033[0m1\033[91m] \033[92mSubdomain created';
 echo -e '\033[91m[\033[0m2\033[91m] \033[92mSubmiter mirror-h';
 echo '';
-read -p "Mau yang ena ena: " here
+read -p "LANJUT: " here
 if [ $here -eq 1 ];
 	then
 		clear && chmod +x subdo.php && php subdo.php
